c7909bf5abebd7b8ae594bbdc09fddc182712134
drummer8001 <drummer8001@ac5ffbb6-15c8-11df-a34c-25a5260b911f>
2010-02-13 20:05:18 +0000

    this is the class that will communicate with the database

---
+++ rit-dbconcepts/rit-dbconcepts/DataAccessLayer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+using rit_dbconcepts.Types;
+
+namespace rit_dbconcepts
+{
+    class DataAccessLayer
+    {
+        string conString = "SERVER=trevor-mack.com;" +
+               "DATABASE=database_concepts;" + "UID=mrt9364;" +
+               "PASSWORD=QzVns;";
+
+        MySqlConnection connection;
+        MySqlCommand command;
+        MySqlDataReader Reader;
+
+        public DataAccessLayer()
+        {
+            connection = new MySqlConnection(conString);
+        }
+
+        public List<Movie> getMovies()
+        {
+            List<Movie> retList = new List<Movie>();
+
+            string getArrStr = "SELECT m.movie_id, m.title, m.genre" +
+                "FROM movie AS m";
+
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = "SELECT title FROM movie;";
+
+            connection.Open();
+            Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                int id = Reader.GetValue(0);
+                String title = Reader.GetValue(1);
+                String genre = Reader.GetValue(2);
+                string[] genres = genre.Split(',');
+                retList.Add(new Movie());
+            }
+            connection.Close();
+            return retList;
+        }
+
+        public List<Movie> getMovieByTitle(String title)
+        {
+            List<Movie> retList = new List<Movie>();
+
+            string getArrStr = "SELECT m.movie_id, m.title, m.genre" +
+                "FROM movie AS m WHERE m.title = " + title + " LIMIT 1";
+
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = "SELECT title FROM movie;";
+
+            connection.Open();
+            Reader = command.ExecuteReader();
+
+            while (Reader.Read())
+            {
+                int id = Reader.GetValue(0);
+                String title = Reader.GetValue(1);
+                String genre = Reader.GetValue(2);
+                string[] genres = genre.Split(',');
+                retList.Add(new Movie());
+            }
+            connection.Close();
+            return retList;
+        }
+        
+    }
+}
