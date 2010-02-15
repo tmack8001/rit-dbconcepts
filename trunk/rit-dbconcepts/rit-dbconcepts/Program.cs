80ef8935f1f03bd5f1f95915d9cf2e1493179b08
Lightningt21 <Lightningt21@ac5ffbb6-15c8-11df-a34c-25a5260b911f>
2010-02-15 15:20:20 +0000

    * rit-dbconcepts/View.cs: * rit-dbconcepts/Program.cs: * rit-dbconcepts/rit-dbconcepts.csproj:

--- trunk/rit-dbconcepts/rit-dbconcepts/Program.cs
+++ trunk/rit-dbconcepts/rit-dbconcepts/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -17,7 +17,7 @@ namespace rit_dbconcepts
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new View());
         }
     }
 }
