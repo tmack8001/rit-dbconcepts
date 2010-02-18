d9a0ecc1168d40ff6574615e9aadccc4d8068f03
retmonster <retmonster@ac5ffbb6-15c8-11df-a34c-25a5260b911f>
2010-02-18 00:04:38 +0000

    - Added a few readers to TypeFactory

--- trunk/rit-dbconcepts/rit-dbconcepts/Types/TypeFactory.cs
+++ trunk/rit-dbconcepts/rit-dbconcepts/Types/TypeFactory.cs
@@ -10,21 +10,63 @@ namespace rit_dbconcepts.Types
 {
     public class TypeFactory
     {
-        
-
-        /*Movie buildMovie(DbDataReader movieReader, DbDataReader distroReader, DbDataReader crewReader)
+        public static Movie readMovie(DbDataReader movieReader)
         {
             int idOrdinal = movieReader.GetOrdinal("movie_id");
             int titleOrdinal = movieReader.GetOrdinal("title");
             int genreOrdinal = movieReader.GetOrdinal("genre");
-            
+            int dateOrdinal = movieReader.GetOrdinal("distribution_date");
 
+            String[] genres = movieReader.GetString(genreOrdinal).Split(',');
+            CastCrewMember[] crew = new CastCrewMember[0];
 
             return new Movie(
                 movieReader.GetInt16(idOrdinal),
-                movieReader.GetString(titleOrdinal)
-                
-                
-        }*/
+                movieReader.GetString(titleOrdinal),
+                movieReader.GetDateTime(dateOrdinal),
+                genres,
+                crew); 
+        }
+
+        public static Person readPerson(DbDataReader personReader)
+        {
+            int idOrdinal = personReader.GetOrdinal("person_id");
+            int firstNameOrdinal = personReader.GetOrdinal("first_name");
+            int lastNameOrdinal = personReader.GetOrdinal("last_name");
+
+
+            return new Person(
+                personReader.GetInt16(idOrdinal),
+                personReader.GetString(firstNameOrdinal),
+                personReader.GetString(lastNameOrdinal));
+        }
+
+        public static Address readAddress(DbDataReader addressReader)
+        {
+            int cityOrdinal = addressReader.GetOrdinal("city");
+            int stateOrdinal = addressReader.GetOrdinal("state");
+            int streetOrdinal = addressReader.GetOrdinal("street");
+            int zipOrdinal = addressReader.GetOrdinal("zipcode");
+
+            return new Address(
+                addressReader.GetString(streetOrdinal),
+                addressReader.GetString(cityOrdinal),
+                addressReader.GetString(streetOrdinal),
+                addressReader.GetString(zipOrdinal));
+        }
+
+        public static Customer readCustomer(DbDataReader customerReader)
+        {
+            Person personData = readPerson(customerReader);
+            Address addressData = readAddress(customerReader);
+
+            int cardOrdinal = customerReader.GetOrdinal("card_number");
+            int expOrdinal = customerReader.GetOrdinal("exp_date");
+
+            return new Customer(personData,
+                customerReader.GetString(cardOrdinal),
+                customerReader.GetDateTime(expOrdinal),
+                addressData);
+        }
     }
 }
